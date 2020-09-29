69cee4529e06da456e5d79b5d1eebb6f8339060f
Godwill700 <cg22chinyemba@gmail.com>
2020-09-29 11:03:10 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const innerContainer = document.querySelector(".inner-container");
+const learnBtn = document.createElement("div");
+learnBtn.className = "learn-more";
+document.body.appendChild(learnBtn);
+// const learnBtn = document.querySelector()
+
+
+// const projectTile = document.querySelectorAll(".project-tile");
+// console.log(projectTile)
+// projectTile.forEach(project => {
+//     project.addEventListener("mouseover", (e) => {
+//         innerContainer.appendChild(learnBtn);
+//         console.log(e.target);
+//     });
+//     // //Event Listener
+
+// });
